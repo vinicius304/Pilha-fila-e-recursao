6f8cc71ab6d36d9cda4d6d47bf9ff202dc2ac81e
vinicius304 <schaefervinicius912@gmail.com>
2025-10-15 19:45:12 -0300

    Create vinicius_ESTRUTURA_SEQUENCIAL_14.py

---
+++ vinicius_ESTRUTURA_SEQUENCIAL_14.py
@@ -0,0 +1,6 @@
+nota_1 = float(input("Digite a nota 1: "))
+nota_2 = float(input("Digite a nota 2: "))
+nota_3 = float(input("Digite a nota 3: "))
+nota_4 = float(input("Digite a nota 4: "))
+média = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+print(f"Média {média}")
