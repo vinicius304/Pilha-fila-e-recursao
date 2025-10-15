978dc7016508a156ede78277ae8fe81c35945357
vinicius304 <schaefervinicius912@gmail.com>
2025-10-15 20:12:21 -0300

    Create vinicius_estrutura_decisao.py

---
+++ vinicius_estrutura_decisao.py
@@ -0,0 +1,6 @@
+número_1=input ("Primeiro número: ")
+número_2=input ("Segundo número: ")
+if número_1 > número_2:
+    print(f"{número_1} é o maior! ")
+elif número_1 < número_2:
+    print(f"{número_2} é o maior! ")
